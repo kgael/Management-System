4d14bbd5e706bd118d217c982a6a9678d0116331
arreolavane07 <arreolaojedavanessa@gmail.com>
2025-10-31 18:17:31 -0600

    Se realizo cambio en frontend

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ children, onClick, danger }) {
   const base = "rounded-lg border px-3 py-1 text-xs shadow-sm ";
-  const cls = danger ? base + "bg-rose-600 text-white hover:bg-rose-700" : base + "bg-white hover:bg-gray-50";
+  const cls = danger ? base + "bg-rose-500 text-white hover:bg-rose-700" : base + "bg-white hover:bg-gray-50";
   return (
     <button onClick={onClick} className={cls}>{children}</button>
   );
